dc9ea9bdd3595df141b02668fce8786832f2da91
aldin <aldinberisa1514@gmail.com>
2021-02-11 23:56:39 +0100

    Create mongoose model for restaurant

---
+++ routes/private/restaurant.js
@@ -0,0 +1,71 @@
+let mongoose = require('mongoose')
+
+let restaurantSchema = new mongoose.Schema({
+    opening_hours: {
+        type: [String],
+        required: true,
+    } ,
+    address: {
+        type: String,
+        required: true,
+    },
+    phone_number:{
+        type: String,
+        required: true,
+    },
+    location:{
+        lat: {
+            type: Number, 
+            required: true
+             
+          },
+        lng: {
+            type: Number,
+            required: true,
+        }
+    },
+    icon:{
+        type: String,
+        required: true,
+    },
+
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    price_level: {
+        type: Number,
+        required: true,
+    },
+
+    rating: {
+        type: Number,
+        required: true,
+    },
+
+    google_maps_url:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    
+    website: {
+        type: String,
+        required: true,
+    },
+
+    photo: {
+        type: String,
+        required: true,
+    },
+
+    id:{
+        type: Number,
+        required: true,
+    }
+
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
