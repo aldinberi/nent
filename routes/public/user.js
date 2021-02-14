21423b09a8319ae87518a736ef0a0dbeab07a903
aldin <aldinberisa1514@gmail.com>
2021-02-14 20:16:53 +0100

    Add support for validating swedish letters

--- routes/public/user.js
+++ routes/public/user.js
@@ -1,5 +1,5 @@
 module.exports = (router, config, jwt) => {
-	const { body, validationResult } = require("express-validator");
+	const { check, validationResult } = require("express-validator");
 	const UserDao = require("../../dao/UserDao");
 	const userModel = require("../../models/mongoose/user");
 	const check_password = require("../../utils");
@@ -7,16 +7,24 @@ module.exports = (router, config, jwt) => {
 
 	router.post(
 		"/register",
-		body("email").isEmail().normalizeEmail(),
-		body("first_name").isAlpha(),
-		body("last_name").isAlpha(),
-		body("email").custom(async (value) => {
+		check("email").isEmail().normalizeEmail(),
+		check("first_name").custom(async (value) => {
+			if (!/^[\sa-zA-ZåäöÅÄÖ ]*$/.test(value)) {
+				return Promise.reject("Invalid first name");
+			}
+		}),
+		check("last_name").custom(async (value) => {
+			if (!/^[\sa-zA-ZåäöÅÄÖ ]*$/.test(value)) {
+				return Promise.reject("Invalid last name");
+			}
+		}),
+		check("email").custom(async (value) => {
 			let user = await userModel.findOne({ email: value });
 			if (user) {
 				return Promise.reject("E-mail already in use");
 			}
 		}),
-		body("password").custom(async (value) => {
+		check("password").custom(async (value) => {
 			let response = await check_password(value);
 			if (response) {
 				return Promise.reject("Password has been breached");
@@ -31,7 +39,7 @@ module.exports = (router, config, jwt) => {
 		}
 	);
 
-	router.post("/login", body("email").isEmail().normalizeEmail(), async (req, res) => {
+	router.post("/login", check("email").isEmail().normalizeEmail(), async (req, res) => {
 		const validationErrors = validationResult(req);
 		if (!validationErrors.isEmpty()) {
 			return res.status(422).json(validationErrors.array());
